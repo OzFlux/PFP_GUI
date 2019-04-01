10ac879664a6aa95bbea46d5196b09fb4ec3ce71
Peter Isaac <pisaac.ozflux@gmail.com>
2019-04-01 14:42:31 +1100

    Include all statistics in MPT output file in Excel workbook.

--- scripts/pfp_mpt.py
+++ scripts/pfp_mpt.py
@@ -12,6 +12,53 @@ import pfp_utils
 
 logger = logging.getLogger("pfp_log")
 
+def get_seasonal_results(contents):
+    # get the seasonal values
+    season_values = numpy.array([float(s) for s in contents[2].split()])
+    season_counts = numpy.array([int(s) for s in contents[3].split()])
+    return {"value": season_values, "count": season_counts}
+
+def get_annual_results(contents):
+    # get the annual values
+    season_values = numpy.array([float(s) for s in contents[2].split()])
+    season_counts = numpy.array([int(s) for s in contents[3].split()])
+    annual_values = numpy.max(season_values)
+    annual_counts = numpy.sum(season_counts)
+    return {"value": annual_values, "count": annual_counts}
+
+def get_temperature_class_results(contents):
+    # get the seasonal results by temperature class
+    temperature_classes = {}
+    for i, n in enumerate(range(7, 14)):
+        temperature_classes[i] = {}
+        temperature_classes[i]["values"] = numpy.array([float(s) for s in contents[n].split()])
+        temperature_classes[i]["counts"] = numpy.zeros(len(temperature_classes[i]["values"]))
+    return temperature_classes
+
+def get_bootstrap_results(contents):
+    # get the individual bootstrap results
+    bootstrap_results = {}
+    bootstrap_values = {}
+    bootstrap_counts = {}
+    n = 17
+    season_values = numpy.array([float(s) for s in contents[n][:contents[n].index("forward")].split()])
+    number_seasons = len(season_values)
+    for i in range(0, number_seasons):
+        bootstrap_values[i] = numpy.array([])
+        bootstrap_counts[i] = numpy.array([])
+    while len(contents[n]) >= 1:
+        season_values = numpy.array([float(s) for s in contents[n][0:contents[n].index("forward")].split()])
+        season_counts = numpy.array([int(s) for s in contents[n+1][0:].split()])
+        for i in range(0, number_seasons):
+            bootstrap_values[i] = numpy.append(bootstrap_values[i], season_values[i])
+            bootstrap_counts[i] = numpy.append(bootstrap_counts[i], season_counts[i])
+        n = n + 2
+    for i in range(0, number_seasons):
+        bootstrap_results[i] = {}
+        bootstrap_results[i]["values"] = bootstrap_values[i]
+        bootstrap_results[i]["counts"] = bootstrap_counts[i]
+    return bootstrap_results
+
 def make_data_array(ds, current_year):
     ldt = pfp_utils.GetVariable(ds, "DateTime")
     nrecs = ds.globalattributes["nc_nrecs"]
@@ -68,6 +115,41 @@ def run_mpt_code(ds, nc_file_name):
     mptlogfile.close()
     return out_file_paths
 
+#def read_mpt_output(out_file_paths):
+    #ustar_results = {"Annual":{}, "Years":{}}
+    #seasons = ["Summer", "Autumn", "Winter", "Spring"]
+    #ura = ustar_results["Annual"]
+    #ury = ustar_results["Years"]
+    #year_list = sorted(out_file_paths.keys())
+    #for year in year_list:
+        #ury[year] = {}
+        #out_file_path = out_file_paths[year]
+        #with open(out_file_path) as file:
+            #lines = file.readlines()
+        ## pick out the annual data
+        #season_values = lines[2].strip().split()
+        #season_count = lines[3].strip().split()
+        ## entry 0 for the year is the observation run, 1 onwards are bootstraps
+        #ury[year][0] = {}
+        #for i in range(len(season_values)):
+            #ury[year][0][seasons[i]] = {"value":float(season_values[i]),"count":int(season_count[i])}
+        #annual_value = max([ury[year][0][season]["value"] for season in ury[year][0].keys()])
+        #annual_count = sum([ury[year][0][season]["count"] for season in ury[year][0].keys()])
+        #ura[year] = {"value":annual_value,"count":annual_count}
+        #n = 17
+        #i = 1
+        #while len(lines[n].strip()) > 0:
+            #ury[year][i] = {}
+            #if "forward" in lines[n]:
+                #lines[n] = lines[n][:lines[n].index("forward")]
+            #season_values = lines[n].strip().split()
+            #season_count = lines[n+1].strip().split()
+            #for j in range(len(season_values)):
+                #ury[year][i][seasons[j]] = {"value":float(season_values[j]),"count":int(season_count[j])}
+            #n += 2
+            #i += 1
+    #return ustar_results
+
 def read_mpt_output(out_file_paths):
     ustar_results = {"Annual":{}, "Years":{}}
     seasons = ["Summer", "Autumn", "Winter", "Spring"]
@@ -77,41 +159,98 @@ def read_mpt_output(out_file_paths):
     for year in year_list:
         ury[year] = {}
         out_file_path = out_file_paths[year]
-        with open(out_file_path) as file:
-            lines = file.readlines()
-        # pick out the annual data
-        season_values = lines[2].strip().split()
-        season_count = lines[3].strip().split()
-        # entry 0 for the year is the observation run, 1 onwards are bootstraps
-        ury[year][0] = {}
-        for i in range(len(season_values)):
-            ury[year][0][seasons[i]] = {"value":float(season_values[i]),"count":int(season_count[i])}
-        annual_value = max([ury[year][0][season]["value"] for season in ury[year][0].keys()])
-        annual_count = sum([ury[year][0][season]["count"] for season in ury[year][0].keys()])
-        ura[year] = {"value":annual_value,"count":annual_count}
-        n = 17
-        i = 1
-        while len(lines[n].strip()) > 0:
-            ury[year][i] = {}
-            if "forward" in lines[n]:
-                lines[n] = lines[n][:lines[n].index("forward")]
-            season_values = lines[n].strip().split()
-            season_count = lines[n+1].strip().split()
-            for j in range(len(season_values)):
-                ury[year][i][seasons[j]] = {"value":float(season_values[j]),"count":int(season_count[j])}
-            n += 2
-            i += 1
+        with open(out_file_path) as mpt_file:
+            contents = [l.rstrip('\n') for l in mpt_file.readlines()]
+        # check the first line to make sure it is what we expect
+        if not "ustar threshold by season" in contents[0] or not "bootstrapping" in contents[15]:
+            msg = "MPT: unexpected contents in MPT output file"
+            logger.error(msg)
+            return ustar_results
+        ury[year]["seasonal"] = get_seasonal_results(contents)
+        ury[year]["annual"] = get_annual_results(contents)
+        ury[year]["temperature_classes"] = get_temperature_class_results(contents)
+        ury[year]["bootstraps"] = get_bootstrap_results(contents)        
     return ustar_results
 
+def write_mpt_year_results(xl_sheet, mpt_results):
+    # write the seasonal results
+    row = 0
+    # write the headers
+    for col, item in enumerate(["Seasonal", "Values", "Counts", "Stdev"]):
+        xl_sheet.write(row, col, item)
+    # write the data
+    for n in range(len(mpt_results["seasonal"]["value"])):
+        row = row + 1
+        xl_sheet.write(row, 0, n)
+        xl_sheet.write(row, 1, mpt_results["seasonal"]["value"][n])
+        xl_sheet.write(row, 2, mpt_results["seasonal"]["count"][n])
+        values = mpt_results["bootstraps"][n]["values"]
+        values = numpy.ma.masked_values(values, -9999)
+        if numpy.ma.count(values) > 0:
+            xl_sheet.write(row, 3, numpy.ma.std(values))
+    # write the temperature class results
+    row = 10
+    # write the headers
+    number_seasons = len(mpt_results["bootstraps"].keys())
+    header_list = ["Temperature classes"]
+    for s in range(number_seasons):
+        header_list.append(str(s))
+    for col, item in enumerate(header_list):
+        xl_sheet.write(row, col, item)
+    # write the data
+    for i in range(len(mpt_results["temperature_classes"].keys())):
+        row = row + 1
+        xl_sheet.write(row, 0, i)
+        for j in range(len(mpt_results["temperature_classes"][0]["values"])):
+            xl_sheet.write(row, j+1, mpt_results["temperature_classes"][i]["values"][j])
+    # write the bootstrap results
+    row = 20
+    # write the headers
+    header1_list = ["Seasons"]
+    header2_list = ["Bootstraps"]
+    for s in range(number_seasons):
+        header1_list.append(str(s))
+        header1_list.append(str(s))
+        header2_list.append("Values")
+        header2_list.append("Counts")
+    for col, item in enumerate(header1_list):
+        xl_sheet.write(row, col, item)
+    row = row + 1
+    for col, item in enumerate(header2_list):
+        xl_sheet.write(row, col, item)
+    # write the data
+    number_bootstraps = len(mpt_results["bootstraps"][0]["values"])
+    for n in range(number_bootstraps):
+        row = row + 1
+        col = 0
+        xl_sheet.write(row, col, n)
+        for s in range(number_seasons):
+            col = col + 1
+            xl_sheet.write(row, col, mpt_results["bootstraps"][s]["values"][n])
+            col = col + 1
+            xl_sheet.write(row, col, mpt_results["bootstraps"][s]["counts"][n])
+    return
+
 def xl_write_mpt(mpt_full_path, ustar_results):
-    year_list = sorted(ustar_results["Annual"].keys())
+    year_list = sorted(ustar_results["Years"].keys())
     xl_file = xlwt.Workbook()
     xl_sheet = xl_file.add_sheet("Annual")
     xl_sheet.write(0, 0,"Year")
     xl_sheet.write(0, 1,"ustar_mean")
+    xl_sheet.write(0, 2,"ustar_sig")
     for n, year in enumerate(year_list):
         xl_sheet.write(n+1, 0, year)
-        if ustar_results["Annual"][year]["value"] != -9999:
-            xl_sheet.write(n+1, 1, ustar_results["Annual"][year]["value"])
+        if ustar_results["Years"][year]["annual"]["value"] != -9999:
+            xl_sheet.write(n+1, 1, ustar_results["Years"][year]["annual"]["value"])
+            season_list = ustar_results["Years"][year]["bootstraps"].keys()
+            values = ustar_results["Years"][year]["bootstraps"][0]["values"]
+            season_list.remove(0)
+            for s in season_list:
+                values = numpy.concatenate((values, ustar_results["Years"][year]["bootstraps"][s]["values"]))
+            values = numpy.ma.masked_values(values, -9999)
+            xl_sheet.write(n+1, 2, numpy.ma.std(values))
+    for year in year_list:
+        xl_sheet = xl_file.add_sheet(str(year))
+        write_mpt_year_results(xl_sheet, ustar_results["Years"][year])
     xl_file.save(mpt_full_path)
     return
