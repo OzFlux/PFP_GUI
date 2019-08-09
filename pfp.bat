91568850a4c973616a26f7a4682e6e00b8ddc9db
Peter Isaac <pisaac.licor@gmail.com>
2019-08-09 12:11:58 +1000

    Modify Windows batch file for batch prcessing.

--- pfp.bat
+++ pfp.bat
@@ -3,6 +3,23 @@ rem
 rem Remember the good old days of DOS batch files ... ?
 rem
 call conda activate pfp_env
+if "%1"=="" goto :interactive
+if "%1"=="batch" goto :batch
+goto :end
+
+:interactive
 python PyFluxPro.py
+goto :end
+
+:batch
+if "%2"=="" call :batch_nocf
+python pfp_batch.py %2
+goto :end
+
+:batch_nocf
+python pfp_batch.py
+goto :end
+
+:end
 call conda deactivate
 @echo on
